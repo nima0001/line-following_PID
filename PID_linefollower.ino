d6b8dd2d3cb28290386eae10ea5dea53299aca9e
Ngima Hyolmo <77228134+nima0001@users.noreply.github.com>
2021-12-07 21:18:18 -0500

    Update intersection case for figure 8

--- PID_linefollower.ino
+++ PID_linefollower.ino
@@ -9,12 +9,31 @@
 
 #include <DRV8835MotorShield.h>
 DRV8835MotorShield motors;
-int leftspeed = 80; 
-int rightspeed = 95;
-//To drive straight, motors need to have slightly different speed because of assymetric bot 
-int v1 = 160;
-int v2 = 135;
 
+//Line following IR sensor:
+int pin_arr[5] = {A1,A2,A3,A4,A5};
+float  y_arr[5]; //for vector valued output from each IR sensors
+const float maxim = 990.0;
+const float minim = 150.0;
+
+//PID Variables:
+int y = 3; //target value 
+float integral = 0.0; 
+float previousError = 0.0;
+
+
+float x = 1.5; //with 1.5, 100 and 40 worked best 
+int leftspeed = 80.0*x;  
+int rightspeed = 95.0*x;
+
+
+float Kp = 140.0*x; //Proportional parameter 
+float Ki = 0.0;     //Integral Parameter
+float Kd = 40.0*x;  //Derivative parameter 
+float delta_t = 1.5;//Time between subsequent change
+
+float t_start;
+float t_end;
 
 
 
@@ -30,32 +49,8 @@ int servoPosition = 96;    // variable to store the servo position
 
 
 
-
-
-
-
-//Line following IR sensor:
-int pin_arr[5] = {A1,A2,A3,A4,A5};
-const float maxim = 990.0;
-const float minim = 150.0;
-
-
-
-//PID Variables:
-int y = 3; //target
-float integral = 0.0;
-float previousError = 0.0;
-
-
-float Kp = 30.0; //Proportional parameter
-float Ki = 400.0;  //Integral Parameter
-float Kd = 40.0; //Derivative parameter
-float delta_t = 100.0; //Time between subsequent change
-
-float t_start;
-float t_end;
-
 void setup() {
+  float t_0 = millis();
   Serial.begin(9600);
   myservo.attach(servo_pin);
   myservo.write(offset+90);
@@ -63,56 +58,61 @@ void setup() {
   for (int i = 0; i<5; i++) {
     pinMode(pin_arr[i],INPUT);
   }
-  
 }
 
 
 
 
+
+
+
+
 void loop () {
-  unsigned long StartTime = micros();
+  float StartTime = micros();
 
   //Calculating sensor output:
   float y_prime = 0.0; //output from sensor (to be updated)
   float sum = 0.0;
   for (int i = 0; i<5; i++) {  
     float yi = ((float)analogRead(pin_arr[i]) - minim)/(maxim-minim); //scaling between 0 and 1
-    
+    y_arr[i] = yi;
     sum += yi;  
     y_prime += (1- yi)*i;  
   }
+
+ 
+
+  //Intersection case:
+  if ((y_arr[0] <= 0.2 and y_arr[2] <= 0.2) or (y_arr[4] <= 0.2 and y_arr[2] <= 0.2)) {
+    motors.setM1Speed(rightspeed);
+    motors.setM2Speed(leftspeed);  
+    delay(300); 
+  }
+  
+
+  //General case:
+  else {
+    y_prime = y_prime/(5 - sum) + 1.0;
+    float error = y - y_prime;  
     
-  y_prime = y_prime/(5 - sum) + 1.0 ;
-  Serial.println(y_prime);
-  Serial.println();
+    //PID:
+    integral += error * delta_t; //right reimann sum or right rectangle piece-wise approx.
+    float derivative = (error - previousError)/delta_t; //forward difference or forward Euler
   
-  float error = y - y_prime;
-
-
-
-
-  //PID:
-   
-  integral += error * delta_t; //defined in global scope
-  float derivative = (error - previousError)/delta_t;
-
-  float del = Kp*error + Ki*integral + Kd*derivative;
-  previousError = error;
-
-
-
-  //MOTOR CONTROL:
-  motors.setM1Speed(rightspeed -del);
-  motors.setM2Speed(leftspeed  +del);
-
-
-
-  //NO CODES DOWN HERE:
-  //MAKING SURE time difference is exactly delta_t
-  unsigned long CurrentTime = micros();
-  unsigned long elapsedTime = ((CurrentTime - StartTime))/1000;
-  Serial.println(elapsedTime);
+    float del = Kp*error + Ki*integral + Kd*derivative;
+    previousError = error;
   
-  delay(delta_t - elapsedTime); //For the difference in times between two subsequent output to be 'exactly' 1000 milliseconds
-   
+ 
+  
+    //MOTOR CONTROL:
+    motors.setM1Speed(rightspeed -del);
+    motors.setM2Speed(leftspeed  +del);
+  
+  
+    //NO CODES DOWN HERE:
+    //MAKING SURE time difference is exactly delta_t
+    float CurrentTime = micros();
+    float elapsedTime = ((CurrentTime - StartTime))/1000.0;
+    delay(delta_t - elapsedTime); //For the difference in times between two subsequent output to be 'exactly' 1000 milliseconds 
+  }
 }
